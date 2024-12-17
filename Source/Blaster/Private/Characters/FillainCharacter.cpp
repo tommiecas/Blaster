5acf00925212ba8e324bdfe0487e3770420c38ea
Tom Casiello <tomcasiello@gmail.com>
2024-12-16 20:40:46 -0800

    Net Update Frequency

--- Source/Blaster/Private/Characters/FillainCharacter.cpp
+++ Source/Blaster/Private/Characters/FillainCharacter.cpp
@@ -49,6 +49,8 @@ AFillainCharacter::AFillainCharacter()
 	GetMesh()->SetCollisionResponseToChannel(ECollisionChannel::ECC_Camera, ECollisionResponse::ECR_Ignore);
 
 	TurningInPlace = ETurningInPlace::ETIP_NotTurning;
+	NetUpdateFrequency = 66.f;
+	MinNetUpdateFrequency = 33.f;
 }
 
 void AFillainCharacter::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
