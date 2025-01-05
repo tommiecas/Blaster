cd4d3e076501457ebdf662a8fe32486e040d12c1
Tom Casiello <tomcasiello@gmail.com>
2025-01-05 09:19:05 -0800

    Syncing  Client and Server Time 2.0

--- Source/Blaster/Public/PlayerController/FillainPlayerController.h
+++ Source/Blaster/Public/PlayerController/FillainPlayerController.h
@@ -26,10 +26,33 @@ public:
 	void SetHUDMatchCountdown(float CountdownTime);
 	virtual void OnPossess(APawn* InPawn) override;
 	virtual void Tick(float DeltaTime) override;
+	virtual float GetServerTime(); // Synced with server World clock
+	virtual void ReceivedPlayer() override;
 
 protected:
 	virtual void BeginPlay() override;
 	void SetHUDTime();
+
+	/**************************************
+	* Sync Time Between Clinet And Server *
+	***************************************/
+
+	//Requests the current server time, passing in the client's time when the request was sent
+	UFUNCTION(Server, Reliable)
+	void ServerRequestServerTime(float TimeOfClientRequest);
+
+	//Reports the current server time to the client in response to ServerRequestServerTime
+	UFUNCTION(Client, Reliable)
+	void ClientReportServerTime(float TimeOfClientRequest, float TimeServerReceivedClientRequest);
+
+	float ClientServerDelta = 0.f; // Difference between Client and Server time
+	
+	UPROPERTY(EditAnywhere, Category = Time)
+	float TimeSyncFrequency = 5.f;
+
+
+	float TimeSyncRunningTime = 0.f;
+	void CheckTimeSync(float DeltaTime);
 private:
 	FString GetWeaponTypeDisplayName(EWeaponType WeaponType);
 
