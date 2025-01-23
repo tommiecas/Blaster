248293341a1914f54d51455bc4e96139975ab8fd
Tom Casiello <tomcasiello@gmail.com>
2025-01-22 16:09:40 -0800

    Grenades in Multiplayer

--- Source/Blaster/Public/Components/CombatComponent.h
+++ Source/Blaster/Public/Components/CombatComponent.h
@@ -50,6 +50,9 @@ public:
 	UFUNCTION(BlueprintCallable)
 	void LaunchGrenade();
 
+	UFUNCTION(Server, Reliable)
+	void ServerLaunchGrenade(const FVector_NetQuantize& Target);
+
 protected:
 	virtual void BeginPlay() override;
 
