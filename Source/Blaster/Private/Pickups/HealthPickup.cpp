fc7475d5b416d455cbd1d02b980b5c8cfe30de58
Tom Casiello <tomcasiello@gmail.com>
2025-01-25 18:31:10 -0800

    Health Pickup

---
+++ Source/Blaster/Private/Pickups/HealthPickup.cpp
@@ -0,0 +1,46 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Pickups/HealthPickup.h"
+#include "Characters/FillainCharacter.h"
+#include "Components/BuffComponent.h"
+#include "NiagaraFunctionLibrary.h"
+#include "NiagaraComponent.h"
+
+AHealthPickup::AHealthPickup()
+{
+	bReplicates = true;
+	PickupEffectComponent = CreateDefaultSubobject<UNiagaraComponent>(TEXT("PickupEffectComponent"));
+	PickupEffectComponent->SetupAttachment(RootComponent);
+}
+
+
+
+void AHealthPickup::OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	Super::OnSphereOverlap(OverlappedComponent, OtherActor, OtherComp, OtherBodyIndex, bFromSweep, SweepResult);
+
+	AFillainCharacter* FillainCharacter = Cast<AFillainCharacter>(OtherActor);
+	if (FillainCharacter)
+	{
+		
+	}
+	Destroy();
+}
+
+void AHealthPickup::Destroyed()
+{
+	Super::Destroyed();
+
+	if (PickupEffect)
+	{
+		UNiagaraFunctionLibrary::SpawnSystemAtLocation(
+			this,
+			PickupEffect,
+			GetActorLocation(),
+			GetActorRotation()
+		);
+	}
+	Super::Destroyed();
+}
+
--- Source/Blaster/Public/Pickups/Pickup.h
+++ Source/Blaster/Public/Pickups/Pickup.h
@@ -24,7 +24,7 @@ protected:
 		class UPrimitiveComponent* OverlappedComponent, 
 		AActor* OtherActor, 
 		UPrimitiveComponent* OtherComp,
-		int32 OtherBodyINdex,
+		int32 OtherBodyIndex,
 		bool bFromSweep,
 		const FHitResult& SweepResult
 	);
